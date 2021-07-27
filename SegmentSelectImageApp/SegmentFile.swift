11790b7532e16815dc2951541ec4a39b34dde9ef
HiroUrata <1712026@i.softbank.jp>
2021-07-27 16:16:26 +0900

    pod install 完了　Main.storyboardでUIの作成完了

---
+++ SegmentSelectImageApp/SegmentFile.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentFile.swift
+//  SegmentSelectImageApp
+//
+//  Created by UrataHiroki on 2021/07/27.
+//
+
+import Foundation
+import UIKit
+
+class SegmentFile{
+    
+    let uiSegmentetControl = UISegmentedControl()
+    
+}
+
+
--- SegmentSelectImageApp/ViewController.swift
+++ SegmentSelectImageApp/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
 
 }
 
