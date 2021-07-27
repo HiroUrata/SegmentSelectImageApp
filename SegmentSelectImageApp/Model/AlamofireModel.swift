7149b2e634a4bf6c7489a189a399fd19e69375f9
HiroUrata <1712026@i.softbank.jp>
2021-07-27 16:28:25 +0900

    AlamofireModelとJsonDataSetsの追加

---
+++ SegmentSelectImageApp/Model/AlamofireModel.swift
@@ -0,0 +1,58 @@
+//
+//  AlamofireModel.swift
+//  SegmentSelectImageApp
+//
+//  Created by UrataHiroki on 2021/07/27.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+
+class AlamofireModel{
+    
+    var jsonResultDatasArray = [JsonDataSets]()
+    
+}
+
+extension AlamofireModel{
+    
+    func searchGetImageURL(searchKeyword:String){
+
+            AF.request("https://pixabay.com/api/?key=22343236-f9a23846f5c6250f4483d03d6&q=\(searchKeyword)", method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+
+                switch response.result{
+
+                case.success:
+
+                    self.jsonResultDatasArray = [] //以前のデータを削除
+
+                    let jsonResult:JSON = JSON(response.data as Any)
+
+                    for needDataCount in 0...15{
+
+                        if jsonResult["hits"][needDataCount]["webformatURL"].string != nil{
+
+                            let jsonResult = JsonDataSets(imageURLData: jsonResult["hits"][needDataCount]["webformatURL"].string!)
+
+                            self.jsonResultDatasArray.append(jsonResult)
+
+                        }else{
+
+                            break  //取得できる"webformatURL"が無くなった時に処理を終わらせる
+
+                        }
+                    }
+
+                case .failure:
+
+                    let error = NSError()
+                    print(error.debugDescription)
+                    break
+
+                }
+            }
+        }
+    
+}
